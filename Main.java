62336e1d3d56410316c3fa1f92e5426e3fa863bd
Wally1605 <156972129+Wally1605@users.noreply.github.com>
2024-02-20 18:51:39 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
+//For creativity, I added color and added addition pop up prompts if the user chooses dinner party.
