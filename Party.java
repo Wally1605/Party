8ee7023dbb910a878a8a8f7197b976a46925d05b
Wally1605 <156972129+Wally1605@users.noreply.github.com>
2024-02-20 18:52:42 -0500

    Create Party.java

---
+++ Party.java
@@ -0,0 +1,14 @@
+public class Party {
+    public int guests;
+
+    public void setGuests(int guests){
+        this.guests = guests;
+    }
+
+    public int getGuests() {
+        return guests;
+    }
+   public void displayInvitation(){
+        System.out.println("Please come to my party!");
+   }
+}
